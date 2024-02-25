02f9fee6494ea6d5149274bf822c7e2ee28de690
abhip1106 <abhishekprabhu04@gmail.com>
2024-02-25 11:48:22 -0800

    HomeWork7 in Java

---
+++ HomeWork/HomeWork8/HomeWork8.java
@@ -0,0 +1,20 @@
+package HomeWork.HomeWork8;
+
+import java.util.*;
+
+public class HomeWork8{
+    public static int minMeetingRooms(int[][] intervals) {
+        HashMap<Integer, Integer> map = new HashMap<Integer,Integer>(); // Key --> Minute Value --> meetings at minute
+        for(int i = 0; i < intervals.length; i++){
+            for(int x = intervals[i][0]; x < intervals[i][1]; x++){
+                map.put(x, map.getOrDefault(x,0) + 1);
+            }
+        }
+        return Collections.max(map.values());
+    }
+    public static void main(String[] args) {
+        int[][] mat = {{0,30},{5,10},{15,20}};
+        System.out.println(minMeetingRooms(mat));
+    }
+
+}
